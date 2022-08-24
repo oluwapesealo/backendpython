e5b45263c018daa7ef4635e4450dbddb42387ae2
Oluwapese Alo <oluwapese.alo@github.com>
2022-08-24 21:25:08 +0100

    Alo

--- Alo/main.py
+++ Alo/main.py
@@ -6,31 +6,33 @@ from flask_restful import Resource, Api, reqparse
 import pypyodbc as odbc 
 import mysql.connector
 from mysql.connector import Error
+import pandas as pd
+import pyodbc
 
 
 app = Flask(__name__)
 api = Api(app)
 
 requests ={}
-class employees(Resource):
+
 #1) Endpoint name -> CreateEndpoint
 #    inputData-> Username, password, department, position
 #    outputData-> token generated
 #     Logic-> Creates new user
-    def __init__(self):
-        self.ID=''
-        self.Email=''
-        self.Password=''
-        self.Department=''
-        self.Line_manager=''
-        self.Team_lead=''
-        self.Employee=''
-        self.create={'Email':str(self.Email), 'Password':str(self.Password),'Department':str(self.Department), 'Line Manager': bool(self.Line_manager),'Team Lead': bool(self.Team_lead), 'Employee':bool(self.Employee)}
+class employees(Resource):
+    def __init__(self, Email='', Password='', Department='', Status=''):
+        self.Email= Email
+        self.Password=Password
+        self.Department=Department
+        self.Status=Status
+        self.create={'Email':str(self.Email), 'Password':str(self.Password),'Department':str(self.Department), 'Line Manager': str(self.Status)}
+
+
     def post(self):
         return(self.create)
-        #function to schedule days
+
     
-    def get(self, create_employee):
+    def post(self, Email='', Password='', Department='', Status=''):
         import pyodbc 
         connect = pyodbc.connect('Driver={SQL Server};'
         'Server=PESES-LAPTOP;'
@@ -38,20 +40,26 @@ class employees(Resource):
         'Trusted_Connection=yes;')
 
         cursor = connect.cursor()
+        # Email = input('Email: ')
+        # Password = input('Password: ')
+        # Department = input('Department: ')
+        # Status = input('Status: ')
+        
 
-        cursor.execute('''INSERT INTO employeetable1 VALUES (4,'oluwapse.lo@wemabank.com', 'peseao','IT',0 , 0, 1, '0', 0, 1, 1, 1, 1, 1)''')
+        cursor.execute('''INSERT INTO employeetable2 VALUES (?,?,?,?,'0', '1', '0', 0, 1, 1, 1, 1, 1)''',(Email, Password, Department, Status))
+
         connect.commit()
         
         return redirect('/')
-api.add_resource(employees,'/createemployee')
+api.add_resource(employees,'/createemployee/<string:Email>/<string:Password>/<string:Department>/<Status>')
 
-class allrequests(Resource):
+
 # 2) Endpoint name -> ShowallRequestsEndpoint
 #    inputData-> Token	
 #    outputData-> Displays all the requests sent by employees
 #     Logic-> Once the endpoint is hit, the app returns the page that shows all the requests received by employees   
-
-    def get(self, allrequest):
+class allrequests(Resource):
+    def get(self):
 
 
         drivername='SQL SERVER'
@@ -73,16 +81,14 @@ class allrequests(Resource):
         requests= finaldatabase.to_dict('records')
         return(requests)
 
-
-
 api.add_resource(allrequests,'/allrequests')
 
-class viewrequests(Resource):
+
 # 3) Endpoint name -> ViewRequestEndpoint
 #    inputData-> Token
 #    outputData-> Shows the selected request
 #     Logic-> Once the endpoint is hit, the app returns the page that shows the selected request
-    
+class viewrequests(Resource):
     def get(self):
         drivername='SQL SERVER'
         servername='PESES-LAPTOP'
@@ -105,15 +111,16 @@ class viewrequests(Resource):
 
 api.add_resource(viewrequests,'/allrequests/viewrequest1')
 
-class teamapproval(Resource):
+
 # 4) Endpoint name -> TeamLeadApprovalEndpoint
 #    inputData-> Token Response give reason for decline
 #    outputData-> 
 #     Logic-> If it is declined, forwards the response to the sender of the request and goes back to the show all requests page 
 # 	      If it is approved, forwards the request to the Line manager and goes back to the show all page
+class teamapproval(Resource):
     def __init__(self):
         self.TeamLead_Approval=''
-        self.Reason_for_Decline=''
+        self.Reason_for_Teamlead_Decline=''
         self.approval={'Approval':bool(self.TeamLead_Approval), 'Reason for Decline':str(self.Reason_for_Decline)}
     
 
@@ -130,11 +137,11 @@ class teamapproval(Resource):
 
             cursor = connect.cursor()
             if self.TeamLead_Approval == True:
-                cursor.execute('''update employeereqs set TeamLead_Approval = 1 where userid =3''')
+                cursor.execute('''update employeereqs set TeamLead_Approval = 1 where userid =1''')
                 connect.commit()
             if self.TeamLead_Approval == False:
-                cursor.execute('''update employeereqs set TeamLead_Approval = 0 where userid =3''') 
-                cursor.execute('''update employeereqs set Reason_for_Decline = "Traffic" where userid =3''')  
+                cursor.execute('''update employeereqs set TeamLead_Approval = 0 where userid =1''') 
+                cursor.execute('''update employeereqs set Reason_for_Teamlead_Decline = "Traffic" where userid =3''')  
             return redirect('/allrequests')
 api.add_resource(teamapproval,'/allrequests/viewrequest1/teamleadapproval')
 
@@ -165,30 +172,46 @@ class linemanager(Resource):
             cursor = connect.cursor()
             if self.LineManager_Approval == True:
                 cursor.execute('''update employeereqs set LineManager_Approval = 1 where userid =3''')
+                cursor.execute('''update employeetable2 set Monday = 1 where userid =3''')
+                cursor.execute('''update employeetable2 set Tuesday = 0 where userid =3''')
+                cursor.execute('''update employeetable2 set Wednesday = 0 where userid =3''')
+                cursor.execute('''update employeetable2 set Thursday = 1 where userid =3''')
+                cursor.execute('''update employeetable2 set Friday = 1 where userid =3''')
                 connect.commit()
             if self.LineManager_Approval == False:
                 cursor.execute('''update employeereqs set LineManager_Approval = 0 where userid =3''') 
-                cursor.execute('''update employeereqs set Reason_for_Decline = "Traffic" where userid =3''')  
+                cursor.execute('''update employeereqs set Reason_for_Linemanager_Decline = "Traffic" where userid =3''')  
             return redirect('/allrequests')
-api.add_resource(linemanager,'/allrequests/viewrequest1/teamleadapproval')
+api.add_resource(linemanager,'/allrequests/viewrequest1/linemanagerapproval')
 
-import pandas as pd
-import pyodbc
- # 6)Endpoint name -> DownloadRequestEndpoint
+
+#  6)Endpoint name -> DownloadRequestEndpoint
 #    inputData-> Token
 #    outputData-> File
 #     Logic-> The request will be downloaded in a csv. format.
-class downloadreq:
-    def get(self, reqdownload):
+
+class downloadreq(Resource):
+    import pandas as pd
+    import pyodbc
+    def get(self):
         connect = pyodbc.connect('Driver={SQL Server};'
             'Server=PESES-LAPTOP;'
             'Database=employeedb;'
             'Trusted_Connection=yes;')
-        reqfile = pd.read_reqfile('''select *from employeereqs where userid = 1''', connect)
-        df = pd.DataFrame(reqfile)
-        df.to_csv()
+        test1 = pd.read_sql_query('''select *from employeereqs where userid = 1''', connect)
+        df = pd.DataFrame(test1)
+        df.to_csv("alo.csv")
         return redirect('/allrequests')
 
-api.add_resource(downloadreq, '/showallrequests/viewrequest/<requestid:str>/linemanagerapproval')
+api.add_resource(downloadreq, '/allrequests/viewrequest1/linemanagerapproval/download')
 
-     
+if __name__ == '__main__':
+    app.run(debug =True) 
+
+#loopholes
+#active directory
+# can not pick days that consecutively follow friday and monday
+# eg. Monday and Friday, Mondays and Tuesday
+# line manager can not approve his own request, needs to be sent to a line manager of another department
+#can not yet select whcich request to view
+#uses static viewing
--- Alo/test.py
+++ Alo/test.py
@@ -1,122 +1,69 @@
-#import pymysql
-
-# connection =pymysql.connect(host='localhost', user='sa', passwd='sa')
-
-# cursor = connection.cursor()
-# sql = "INSERT INTO `employeetb` (`userid`,`Firstname`,`Lastname`,`staffID`) VALUES(%s, %s, %s, %s)"
-
-# cursor.execute(sql, (5,'Pese','Alo','IND19'))
-# connection.commit()
-
-# sql = "SELECT FROM `employeetb`"
-# cursor.execute(sql)
-
-# result = cursor.fetchall()
-# for i in result:
-#     print(i)
-# from msilib.schema import SelfReg
-# import urllib
-# from xmlrpc.client import Boolean, boolean
-# from sqlalchemy import create_engine
-# import pypyodbc as odbc 
-# import pandas as pd
-# from flask import Flask,jsonify
-# from flask_restful import Api,Resource 
-# def get(self):
-#         #function to submit days
-#         SERVER='DESKTOP-IEVPBEO'
-#         DATABASE='employeedb'
-#         DRIVER='SQL Server Native Client 11.0'
-#         USERNAME='chidubem'
-#         PASSWORD='ogbuefi'
-#          #connecting to the databasE
-#         connection_string=f'mssql://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver={DRIVER}'
-#         engine = create_engine(connection_string)
-#         connection=engine.connect()
-#         finalogb=pd.read_sql_query('''SELECT * FROM [employeedb].[chidubem].[employe]''',connection)
-#         #modifying the days
-#         finalogb.iloc[0,5]=int(bool(self.Monday))
-#         finalogb.iloc[0,6]=int (bool (self.Tuesday))
-#         finalogb.iloc[0,7]=int(bool (self.Wednesday))
-#         finalogb.iloc[0,8]=int(bool (self.Thursday))
-#         finalogb.iloc[0,9]=int(bool (self.Friday))
-#         #storing the new data in sql
-#         finalogb.to_sql('employe',con=engine,if_exists='replace',index=False)
-#         return(self.days)
-# get(self)
-
-import urllib
-from urllib import response
-from xmlrpc.client import Boolean, boolean
-from sqlalchemy import create_engine
+import email
+from sqlite3 import Cursor
+from flask import Flask, request, jsonify, flash, redirect
+from flask_restful import Resource, Api, reqparse
 import pypyodbc as odbc 
+import mysql.connector
+from mysql.connector import Error
 import pandas as pd
-from flask import Flask,jsonify,redirect
-from flask_restful import Api,Resource
-import requests
-app=Flask(__name__)
-api=Api(app)
-SERVER='PESES-LAPTOP'
-DATABASE='beginnercmd'
-DRIVER='SQL Server Native Client 11.0'
-USERNAME='sa'
-PASSWORD='sa'
-class scheduledays(Resource):
-    def __init__(self):
-        self.Monday=''
-        self.Tuesday=''
-        self.Wednesday=''
-        self.Thursday=''
-        self.Friday=''
-        self.days={'Monday':bool(self.Monday),'Tuesday':bool(self.Tuesday),'Wednesday':bool(self.Wednesday),'Thursday':bool(self.Thursday),'Friday':bool(self.Friday)}
-    def post(self):
-        return(self.days)
-        #function to schedule days
-    def get(self):
-        self.Monday='1'
-        self.Tuesday='1'
-        self.Wednesday='1'
-        self.Thursday='1'
-        self.Friday='1'
-         #connecting to the databasE
-        self.connection_string=f'mssql://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver={DRIVER}'
-        self.engine = create_engine(self.connection_string)
-        self.connection=self.engine.connect()
-        finalogb=pd.read_sql_query('''SELECT * FROM [employeedb].[dbo].[employeetable1]''',self.connection)
-        #modifying the days
-        finalogb.iloc[0,5]=int(bool(self.Monday))
-        finalogb.iloc[0,6]=int (bool (self.Tuesday))
-        finalogb.iloc[0,7]=int(bool (self.Wednesday))
-        finalogb.iloc[0,8]=int(bool (self.Thursday))
-        finalogb.iloc[0,9]=int(bool (self.Friday))
-        #storing the new data in sql
-        finalogb.to_sql('employeetable1',con=self.engine,if_exists='replace',index=False)
-        self.days={'Monday':bool(self.Monday),'Tuesday':bool(self.Tuesday),'Wednesday':bool(self.Wednesday),'Thursday':bool(self.Thursday),'Friday':bool(self.Friday)}
-        return(self.days)
-    def patch(self):
-        self.connection_string=f'mssql://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver={DRIVER}'
-        self.engine = create_engine(self.connection_string)
-        self.connection=self.engine.connect()
-        finalogb=pd.read_sql_query('''SELECT * FROM [employeedb].[dbo].[employeetable1]''',self.connection)
-        #modifying the days
-        finalogb.iloc[0,5]=int(bool(self.Monday))
-        finalogb.iloc[0,6]=int (bool (self.Tuesday))
-        finalogb.iloc[0,7]=int(bool (self.Wednesday))
-        finalogb.iloc[0,8]=int(bool (self.Thursday))
-        finalogb.iloc[0,9]=int(bool (self.Friday))
-        #storing the new data in sql
-        finalogb.to_sql('employeetable1',con=self.engine,if_exists='replace',index=False)
-        self.days={'Monday':bool(self.Monday),'Tuesday':bool(self.Tuesday),'Wednesday':bool(self.Wednesday),'Thursday':bool(self.Thursday),'Friday':bool(self.Friday)}
-        return(self.days)
+import pyodbc
 
-api.add_resource(scheduledays,'/scheduleddays')
-class sqlconnector(Resource):
+
+app = Flask(__name__)
+api = Api(app)
+
+requests ={}
+
+#1) Endpoint name -> CreateEndpoint
+#    inputData-> Username, password, department, position
+#    outputData-> token generated
+#     Logic-> Creates new user
+class employees(Resource):
+    def __init__(self, Email='', Password='', Department='', Status=''):
+        self.Email= Email
+        self.Password=Password
+        self.Department=Department
+        self.Status=Status
+        self.create={'Email':str(self.Email), 'Password':str(self.Password),'Department':str(self.Department), 'Line Manager': str(self.Status)}
+
+
+    def post(self):
+        return(self.create)
+
+    
+    def post(self, Email='', Password='', Department='', Status=''):
+        import pyodbc 
+        connect = pyodbc.connect('Driver={SQL Server};'
+        'Server=PESES-LAPTOP;'
+        'Database=employeedb;'
+        'Trusted_Connection=yes;')
+
+        cursor = connect.cursor()
+        # Email = input('Email: ')
+        # Password = input('Password: ')
+        # Department = input('Department: ')
+        # Status = input('Status: ')
+        
+
+        cursor.execute('''INSERT INTO employeetable2 VALUES (?,?,?,?,'0', '1', '0', 0, 1, 1, 1, 1, 1)''',(Email, Password, Department, Status))
+
+        connect.commit()
+        
+        return redirect('/')
+api.add_resource(employees,'/createemployee/<string:Email>/<string:Password>/<string:Department>/<Status>')
+
+
+# 2) Endpoint name -> ShowallRequestsEndpoint
+#    inputData-> Token	
+#    outputData-> Displays all the requests sent by employees
+#     Logic-> Once the endpoint is hit, the app returns the page that shows all the requests received by employees   
+class allrequests(Resource):
     def get(self):
-        #sql database
+
 
         drivername='SQL SERVER'
         servername='PESES-LAPTOP'
-        database='employeerq'
+        database='employeedb'
         connection_string=f"""
          DRIVER={{{drivername}}};
          SERVER={servername};
@@ -132,42 +79,58 @@ class sqlconnector(Resource):
         #coverting the table to a dictionar
         requests= finaldatabase.to_dict('records')
         return(requests)
-api.add_resource(sqlconnector,"/allrequests")
 
-class viewrequests(Resource):
+api.add_resource(allrequests,'/allrequests')
+
+
 # 3) Endpoint name -> ViewRequestEndpoint
 #    inputData-> Token
 #    outputData-> Shows the selected request
 #     Logic-> Once the endpoint is hit, the app returns the page that shows the selected request
-    
-    def get(self):
-        drivername='SQL SERVER'
-        servername='PESES-LAPTOP'
-        database='employeerq'
-        connection_string=f"""
-         DRIVER={{{drivername}}};
-         SERVER={servername};
-        DATABASE={database};
-        Trust_Connection=yes;
-            """ 
-        #connecting to the database
-        readdata=odbc.connect(connection_string)
-        #to read from sql database
-        SQL_Query=pd.read_sql_query('''select *from employeereqs where userid = 1''',readdata)
-        #storing sql database in python
-        finaldatabase=SQL_Query.head()
-        #coverting the table to a dictionar
-        vrequests= finaldatabase.to_dict('records')
-        return(vrequests)
+class viewrequests(Resource):
+    def get(self,Email=''):
+        self.Email = Email
+        connect = pyodbc.connect('Driver={SQL Server};'
+            'Server=PESES-LAPTOP;'
+            'Database=employeedb;'
+            'Trusted_Connection=yes;')
 
-api.add_resource(viewrequests,'/allrequests/viewrequest1')
+        cursor = connect.cursor()
+        vrequests = cursor.execute('''select *from employeereqs where Email = ?''', self.Email)
+        connect.commit()
 
-class teamapproval(Resource):
+        return (vrequests)
+
+        # drivername='SQL SERVER'
+        # servername='PESES-LAPTOP'
+        # database='employeerq'
+        # connection_string=f"""
+        #  DRIVER={{{drivername}}};
+        #  SERVER={servername};
+        # DATABASE={database};
+        # Trust_Connection=yes;
+        #     """ 
+        # #connecting to the database
+        # readdata=odbc.connect(connection_string)
+        # #to read from sql database
+        # SQL_Query=pd.read_sql_query('''select *from employeereqs where userid = 1''',readdata)
+        # #storing sql database in python
+        # finaldatabase=SQL_Query.head()
+        # #coverting the table to a dictionar
+        # vrequests= finaldatabase.to_dict('records')
+        # return(vrequests)
+        
+        
+
+api.add_resource(viewrequests,'/allrequests/viewrequest/<string:Email>')
+
+
 # 4) Endpoint name -> TeamLeadApprovalEndpoint
 #    inputData-> Token Response give reason for decline
 #    outputData-> 
 #     Logic-> If it is declined, forwards the response to the sender of the request and goes back to the show all requests page 
 # 	      If it is approved, forwards the request to the Line manager and goes back to the show all page
+class teamapproval(Resource):
     def __init__(self):
         self.TeamLead_Approval=''
         self.Reason_for_Decline=''
@@ -177,9 +140,8 @@ class teamapproval(Resource):
         # return(self.approval)
 
     
-    def patch(self):
-        
-            import pyodbc 
+    def patch(self, Email=''):
+            self.Email=Email
             connect = pyodbc.connect('Driver={SQL Server};'
             'Server=PESES-LAPTOP;'
             'Database=employeedb;'
@@ -187,14 +149,19 @@ class teamapproval(Resource):
 
             cursor = connect.cursor()
             if self.TeamLead_Approval == True:
-                cursor.execute('''update employeereqs set TeamLead_Approval = 1 where userid =3''')
+                cursor.execute('''update employeereqs set TeamLead_Approval = 1 where Email =?''', self.Email)
                 connect.commit()
             if self.TeamLead_Approval == False:
-                cursor.execute('''update employeereqs set TeamLead_Approval = 0 where userid =3''') 
-                cursor.execute('''update employeereqs set Reason_for_Decline = "Traffic" where userid =3''')  
+                cursor.execute('''update employeereqs set TeamLead_Approval = 0 where Email =?''', self.Email) 
+                cursor.execute('''update employeereqs set Reason_for_Decline = "Traffic" where Email =?''', self.Email)  
             return redirect('/allrequests')
-api.add_resource(teamapproval,'/allrequests/viewrequest1/teamleadapproval')
+api.add_resource(teamapproval,'/allrequests/viewrequest/<string:Email>/teamleadapproval')
 
+# 5) Endpoint name -> LineManagerApprovalEndpoint
+#    inputData-> Response and give reason for decline
+#    outputData-> gives the option to download the request
+#     Logic-> If it is denied, forwards the response to the sender of the request and goes back to the show all requests page 
+# 	      If it is approved, gives the user the option to download the approved request  
 
 class linemanager(Resource):
     def __init__(self):
@@ -206,9 +173,8 @@ class linemanager(Resource):
         # return(self.approval)
 
     
-    def patch(self):
-        
-            import pyodbc 
+    def patch(self, Email=''):
+            self.Email=Email
             connect = pyodbc.connect('Driver={SQL Server};'
             'Server=PESES-LAPTOP;'
             'Database=employeedb;'
@@ -216,26 +182,44 @@ class linemanager(Resource):
 
             cursor = connect.cursor()
             if self.LineManager_Approval == True:
-                cursor.execute('''update employeereqs set LineManager_Approval = 1 where userid =3''')
+                cursor.execute('''update employeereqs set LineManager_Approval = 1 where Email =?''', self.Email)
                 connect.commit()
             if self.LineManager_Approval == False:
-                cursor.execute('''update employeereqs set LineManager_Approval = 0 where userid =3''') 
-                cursor.execute('''update employeereqs set Reason_for_Decline = "Traffic" where userid =3''')  
+                cursor.execute('''update employeereqs set LineManager_Approval = 0 where Email =?''', self.Email) 
+                cursor.execute('''update employeereqs set Reason_for_Decline = "Traffic" where Email =?''', self.Email)  
             return redirect('/allrequests')
-api.add_resource(linemanager,'/allrequests/viewrequest1/teamleadapproval')
+api.add_resource(linemanager,'/allrequests/viewrequest/<string:Email>/linemanagerapproval')
 
 
+#  6)Endpoint name -> DownloadRequestEndpoint
+#    inputData-> Token
+#    outputData-> File
+#     Logic-> The request will be downloaded in a csv. format.
 
-if __name__ =="__main__":
-    app.run(debug=True)
+class downloadreq(Resource):
+    import pandas as pd
+    import pyodbc
+    def get(self,Email=''):
+        self.Email=Email
+        connect = pyodbc.connect('Driver={SQL Server};'
+            'Server=PESES-LAPTOP;'
+            'Database=employeedb;'
+            'Trusted_Connection=yes;')
+        test1 = pd.read_sql_query('''select *from employeereqs where Email = ?''', self.Email)
+        df = pd.DataFrame(test1)
+        df.to_csv("alo.csv")
+        return redirect('/allrequests')
 
-# import pyodbc 
-# connect = pyodbc.connect('Driver={SQL Server};'
-# 'Server=PESES-LAPTOP;'
-# 'Database=employeedb;'
-# 'Trusted_Connection=yes;')
+api.add_resource(downloadreq, '/allrequests/viewrequest1/linemanagerapproval/download')
 
-# cursor = connect.cursor()
+     
 
-# cursor.execute('''INSERT INTO employeetable1 VALUES (4,'oluwapse.lo@wemabank.com', 'peseao','IT',0 , 0, 1, '0', 0, 1, 1, 1, 1, 1)''')
-# connect.commit()
+#loopholes
+#active directory
+# can not pick days that consecutively follow friday and monday
+# eg. Monday and Friday, Mondays and Tuesday
+# line manager can not approve his own request, needs to be sent to a line manager of another department
+#can not yet select whcich request to view
+#uses static viewing
+if __name__ == '__main__':
+    app.run(debug =True)
