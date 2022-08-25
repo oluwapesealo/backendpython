fc15c42af1780cf31521e06a185855f08f916a52
Oluwapese Alo <oluwapese.alo@github.com>
2022-08-25 15:55:13 +0100

    Alo

--- Alo/main.py
+++ Alo/main.py
@@ -1,5 +1,5 @@
-
 import email
+import json
 from sqlite3 import Cursor
 from flask import Flask, request, jsonify, flash, redirect
 from flask_restful import Resource, Api, reqparse
@@ -25,14 +25,10 @@ class employees(Resource):
         self.Password=Password
         self.Department=Department
         self.Status=Status
-        self.create={'Email':str(self.Email), 'Password':str(self.Password),'Department':str(self.Department), 'Line Manager': str(self.Status)}
 
-
-    def post(self):
-        return(self.create)
 
     
-    def post(self, Email='', Password='', Department='', Status=''):
+    def post(self, Email, Password, Department, Status):
         import pyodbc 
         connect = pyodbc.connect('Driver={SQL Server};'
         'Server=PESES-LAPTOP;'
@@ -46,12 +42,12 @@ class employees(Resource):
         # Status = input('Status: ')
         
 
-        cursor.execute('''INSERT INTO employeetable2 VALUES (?,?,?,?,'0', '1', '0', 0, 1, 1, 1, 1, 1)''',(Email, Password, Department, Status))
+        cursor.execute('''INSERT INTO employeetable2 VALUES (?,?,?,?,'NULL', 'NULL', 'NULL', 0, 0, 0, 0, 0)''',(Email, Password, Department, Status))
 
         connect.commit()
         
         return redirect('/')
-api.add_resource(employees,'/createemployee/<string:Email>/<string:Password>/<string:Department>/<Status>')
+api.add_resource(employees,'/createemployee/<string:Email>/<string:Password>/<string:Department>/<string:Status>')
 
 
 # 2) Endpoint name -> ShowallRequestsEndpoint
@@ -64,7 +60,7 @@ class allrequests(Resource):
 
         drivername='SQL SERVER'
         servername='PESES-LAPTOP'
-        database='employeerq'
+        database='employeedb'
         connection_string=f"""
          DRIVER={{{drivername}}};
          SERVER={servername};
@@ -88,11 +84,29 @@ api.add_resource(allrequests,'/allrequests')
 #    inputData-> Token
 #    outputData-> Shows the selected request
 #     Logic-> Once the endpoint is hit, the app returns the page that shows the selected request
+# requests={}
 class viewrequests(Resource):
-    def get(self):
+    def __init__(self, Email=''):
+        self.Email= Email
+
+    def get(self,Email=''):
+        # requests=[]
+        # connect = pyodbc.connect('Driver={SQL Server};'
+        #     'Server=PESES-LAPTOP;'
+        #     'Database=employeedb;'
+        #     'Trusted_Connection=yes;')
+
+        # cursor = connect.cursor()
+        # cursor.execute('''select *from employeereqs where Email = ?''', (Email))
+        # vrequests=cursor.fetchall()
+        # for request in vrequests:
+        #     requests.append([x for x in request])
+        # return json.dumps(requests)
+
+
         drivername='SQL SERVER'
         servername='PESES-LAPTOP'
-        database='employeerq'
+        database='employeedb'
         connection_string=f"""
          DRIVER={{{drivername}}};
          SERVER={servername};
@@ -102,14 +116,16 @@ class viewrequests(Resource):
         #connecting to the database
         readdata=odbc.connect(connection_string)
         #to read from sql database
-        SQL_Query=pd.read_sql_query('''select *from employeereqs where userid = 1''',readdata)
+        SQL_Query=pd.read_sql_query('''select *from employeereqs where Email ='''+Email, readdata,)
         #storing sql database in python
         finaldatabase=SQL_Query.head()
         #coverting the table to a dictionar
         vrequests= finaldatabase.to_dict('records')
         return(vrequests)
+        
+        
 
-api.add_resource(viewrequests,'/allrequests/viewrequest1')
+api.add_resource(viewrequests,'/allrequests/viewrequest/<string:Email>')
 
 
 # 4) Endpoint name -> TeamLeadApprovalEndpoint
@@ -120,16 +136,15 @@ api.add_resource(viewrequests,'/allrequests/viewrequest1')
 class teamapproval(Resource):
     def __init__(self):
         self.TeamLead_Approval=''
-        self.Reason_for_Teamlead_Decline=''
+        self.Reason_for_Decline=''
         self.approval={'Approval':bool(self.TeamLead_Approval), 'Reason for Decline':str(self.Reason_for_Decline)}
     
 
         # return(self.approval)
 
     
-    def patch(self):
-        
-            import pyodbc 
+    def patch(self, Email=''):
+            self.Email=Email
             connect = pyodbc.connect('Driver={SQL Server};'
             'Server=PESES-LAPTOP;'
             'Database=employeedb;'
@@ -137,13 +152,13 @@ class teamapproval(Resource):
 
             cursor = connect.cursor()
             if self.TeamLead_Approval == True:
-                cursor.execute('''update employeereqs set TeamLead_Approval = 1 where userid =1''')
+                cursor.execute('''update employeereqs set TeamLead_Approval = 1 where Email =?''', self.Email)
                 connect.commit()
             if self.TeamLead_Approval == False:
-                cursor.execute('''update employeereqs set TeamLead_Approval = 0 where userid =1''') 
-                cursor.execute('''update employeereqs set Reason_for_Teamlead_Decline = "Traffic" where userid =3''')  
+                cursor.execute('''update employeereqs set TeamLead_Approval = 0 where Email =?''', self.Email) 
+                cursor.execute('''update employeereqs set Reason_for_Decline = "Traffic" where Email =?''', self.Email)  
             return redirect('/allrequests')
-api.add_resource(teamapproval,'/allrequests/viewrequest1/teamleadapproval')
+api.add_resource(teamapproval,'/allrequests/viewrequest/<string:Email>/teamleadapproval')
 
 # 5) Endpoint name -> LineManagerApprovalEndpoint
 #    inputData-> Response and give reason for decline
@@ -161,9 +176,8 @@ class linemanager(Resource):
         # return(self.approval)
 
     
-    def patch(self):
-        
-            import pyodbc 
+    def patch(self, Email=''):
+            self.Email=Email
             connect = pyodbc.connect('Driver={SQL Server};'
             'Server=PESES-LAPTOP;'
             'Database=employeedb;'
@@ -171,18 +185,13 @@ class linemanager(Resource):
 
             cursor = connect.cursor()
             if self.LineManager_Approval == True:
-                cursor.execute('''update employeereqs set LineManager_Approval = 1 where userid =3''')
-                cursor.execute('''update employeetable2 set Monday = 1 where userid =3''')
-                cursor.execute('''update employeetable2 set Tuesday = 0 where userid =3''')
-                cursor.execute('''update employeetable2 set Wednesday = 0 where userid =3''')
-                cursor.execute('''update employeetable2 set Thursday = 1 where userid =3''')
-                cursor.execute('''update employeetable2 set Friday = 1 where userid =3''')
+                cursor.execute('''update employeereqs set LineManager_Approval = 1 where Email =?''', self.Email)
                 connect.commit()
             if self.LineManager_Approval == False:
-                cursor.execute('''update employeereqs set LineManager_Approval = 0 where userid =3''') 
-                cursor.execute('''update employeereqs set Reason_for_Linemanager_Decline = "Traffic" where userid =3''')  
+                cursor.execute('''update employeereqs set LineManager_Approval = 0 where Email =?''', self.Email) 
+                cursor.execute('''update employeereqs set Reason_for_Decline = "Traffic" where Email =?''', self.Email)  
             return redirect('/allrequests')
-api.add_resource(linemanager,'/allrequests/viewrequest1/linemanagerapproval')
+api.add_resource(linemanager,'/allrequests/viewrequest/<string:Email>/linemanagerapproval')
 
 
 #  6)Endpoint name -> DownloadRequestEndpoint
@@ -191,23 +200,30 @@ api.add_resource(linemanager,'/allrequests/viewrequest1/linemanagerapproval')
 #     Logic-> The request will be downloaded in a csv. format.
 
 class downloadreq(Resource):
+
+    def __init__(self, Email=''):
+        self.Email= Email
     import pandas as pd
     import pyodbc
-    def get(self):
+    def get(self,Email=''):
+        self.Email=Email
+        
         connect = pyodbc.connect('Driver={SQL Server};'
             'Server=PESES-LAPTOP;'
             'Database=employeedb;'
             'Trusted_Connection=yes;')
-        test1 = pd.read_sql_query('''select *from employeereqs where userid = 1''', connect)
+        test1 = pd.read_sql_query('''select *from employeereqs where Email = '''+Email, connect)
         df = pd.DataFrame(test1)
-        df.to_csv("alo.csv")
+        df.to_csv(Email+".csv")
         return redirect('/allrequests')
 
-api.add_resource(downloadreq, '/allrequests/viewrequest1/linemanagerapproval/download')
+api.add_resource(downloadreq, '/allrequests/viewrequest/<string:Email>/linemanagerapproval/download')
+
+     
+
 
 if __name__ == '__main__':
-    app.run(debug =True) 
-
+    app.run(debug =True)
 #loopholes
 #active directory
 # can not pick days that consecutively follow friday and monday
